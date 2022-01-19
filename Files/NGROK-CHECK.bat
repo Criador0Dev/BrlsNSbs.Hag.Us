e6486c2cd7438b1272ac2159b7cc38c96e5dde8a
LuisEdits <83842020+LuisEdits@users.noreply.github.com>
2022-01-19 05:26:33 -0300

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -13,6 +13,8 @@ echo Pass: @LuisEdits2022
 curl -O https://raw.githubusercontent.com/jrrodriguez81/Windows2019RDP-US/main/Files/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/jrrodriguez81/Windows2019RDP-US/main/Files/FastConfigVPS_v5.1.exe > out.txt 2>&1
 del /f "C:\Users\Public\Desktop\Fast Config VPS.exe" > out.txt 2>&1
+del /f "C:\Users\Public\Desktop\Unity Hub.lnk" > out.txt 2>&1
+del /f "C:\Users\Public\Desktop\Google Cloud SDK Shell.lnk" > out.txt 2>&1
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './DisablePasswordComplexity.ps1'" > out.txt 2>&1
 diskperf -Y >nul
 sc start audiosrv >nul
